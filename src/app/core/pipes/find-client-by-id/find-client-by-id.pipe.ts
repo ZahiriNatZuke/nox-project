079c47ecd919f859b102415220a5d1e416f3f879
Yohan González Almaguer <yohan.gonzalez.almaguer@gmail.com>
2024-10-29 10:10:51 -0300

    feat: add uuid validation to find client by id pipe

--- src/app/core/pipes/find-client-by-id/find-client-by-id.pipe.ts
+++ src/app/core/pipes/find-client-by-id/find-client-by-id.pipe.ts
@@ -1,11 +1,15 @@
 import { ClientService } from '@app/features/client/client.service';
 import { Injectable, NotFoundException, PipeTransform } from '@nestjs/common';
+import { z } from 'zod';
 
 @Injectable()
 export class FindClientByIdPipe implements PipeTransform {
 	constructor(private clientService: ClientService) {}
 
 	async transform(id: string) {
+		const result = z.string().uuid('Invalid UUID').safeParse(id);
+		if (!result.success) throw new NotFoundException(result.error.errors);
+
 		try {
 			return await this.clientService.findOne({ id }, true);
 		} catch (_) {
